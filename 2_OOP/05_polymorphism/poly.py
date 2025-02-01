1b1faf99c31afe3e44f4a4704a144d27405fded3
John Sims <johnsims.dev@gmail.com>
2025-02-01 17:25:06 -0500

    finish polymorphism

---
+++ 2_OOP/05_polymorphism/poly.py
@@ -0,0 +1,89 @@
+class Unit:
+    def __init__(self, name, pos_x, pos_y):
+        self.name = name
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+
+    def in_area(self, x1, y1, x2, y2):
+        pass
+
+
+class Dragon(Unit):
+    def __init__(self, name, pos_x, pos_y, height, width, fire_range):
+        super().__init__(name, pos_x, pos_y)
+        self.fire_range = fire_range
+        self.height = height
+        self.width = width
+        self.__hit_box = Rectangle(
+            pos_x - width / 2, pos_y - height / 2, pos_x + width / 2, pos_y + height / 2
+        )
+
+    def in_area(self, x1, y1, x2, y2):
+        newRect = Rectangle(x1, y1, x2, y2)
+        return newRect.overlaps(self.__hit_box)
+
+
+# Don't touch below this line
+
+
+class Rectangle:
+    def overlaps(self, rect):
+        return (
+            self.get_left_x() <= rect.get_right_x()
+            and self.get_right_x() >= rect.get_left_x()
+            and self.get_top_y() >= rect.get_bottom_y()
+            and self.get_bottom_y() <= rect.get_top_y()
+        )
+
+    def __init__(self, x1, y1, x2, y2):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__x2 = x2
+        self.__y2 = y2
+
+    def get_left_x(self):
+        if self.__x1 < self.__x2:
+            return self.__x1
+        return self.__x2
+
+    def get_right_x(self):
+        if self.__x1 > self.__x2:
+            return self.__x1
+        return self.__x2
+
+    def get_top_y(self):
+        if self.__y1 > self.__y2:
+            return self.__y1
+        return self.__y2
+
+    def get_bottom_y(self):
+        if self.__y1 < self.__y2:
+            return self.__y1
+        return self.__y2
+
+
+class Sword:
+    def __init__(self, sword_type):
+        self.sword_type = sword_type
+
+    def __add__(self, other):
+        br = "bronze"
+        ir = "iron"
+        stl = "steel"
+        if self.sword_type == br:
+            if other.sword_type == br:
+                return Sword(ir)
+        if self.sword_type == ir:
+            if other.sword_type == ir:
+                return Sword(stl)
+        else:
+            raise Exception("cannot craft")
+
+
+class Dragon:
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def __str__(self):
+        return f"I am {self.name}, the {self.color} dragon"
