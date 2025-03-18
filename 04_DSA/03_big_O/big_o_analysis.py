02596151cd91026d54d54064450968f03d2c5bc3
simzeee <jwsims816@gmail.com>
2025-03-17 21:23:30 -0400

    finish big_o

--- 04_DSA/03_big_O/big_o_analysis.py
+++ 04_DSA/03_big_O/big_o_analysis.py
@@ -24,3 +24,17 @@ def find_last_name(names_dict, first_name):
         return names_dict[first_name]
     else:
         return None
+
+
+def binary_search(target, arr):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        median = (low + high) // 2
+        if arr[median] == target:
+            return True
+        elif arr[median] < target:
+            low = median + 1
+        else:
+            high = median - 1
+    return False
